c03f6e3074efad975484ffb5e86af7b7006aec8c
r00tus3r <iamakshayajayan@gmail.com>
2018-09-07 21:54:23 +0530

    adding helloworld and sol A

---
+++ Parallel/A.c
@@ -0,0 +1,20 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main() {
+    MPI_Init(NULL, NULL);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    printf("This is process %d of %d \n", world_rank, world_size);
+
+    MPI_Finalize();
+}
